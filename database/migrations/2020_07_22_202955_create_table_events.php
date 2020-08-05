cc264c35c3e88a6cea113cbaad98a02949ca2562
Dionatan Melo <dionatanatlas@gmail.com>
2020-08-05 16:55:25 -0300

    :sparkles: add event migration

---
+++ database/migrations/2020_07_22_202955_create_table_events.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableEvents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('location');
+            $table->text('description');
+            $table->float('price', 8, 2);
+            $table->datetime('event_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
